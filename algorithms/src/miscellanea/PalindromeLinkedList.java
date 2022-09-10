a7ccd2115e6c7bfff91cbce5e228fe6958c9b513
Oleksandr Klymenko <ikytosan0@gmail.com>
2022-09-10 19:38:12 +0300

    solved LeetCode tasks 13 and 234

---
+++ algorithms/src/miscellanea/PalindromeLinkedList.java
@@ -0,0 +1,55 @@
+package miscellanea;
+
+public class PalindromeLinkedList {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1, new ListNode(3, new ListNode(2, new ListNode(1))));
+        Solution solution = new Solution();
+        solution.isPalindrome(head);
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+
+    public boolean isPalindrome(ListNode head) {
+        ListNode fast, slow;
+        fast = slow = head;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        ListNode prev = null;
+        while (slow.next != null) {
+            slow = slow.next;
+            prev = new ListNode(slow.val, prev);
+        }
+        while (prev != null) {
+            if (head.val != prev.val){
+                System.out.println(false);
+                return false;
+            }
+            prev = prev.next;
+            head = head.next;
+        }
+        System.out.println(true);
+        return true;
+    }
+
+
+}
---
+++ algorithms/src/miscellanea/RomanToInt.java
@@ -0,0 +1,49 @@
+package miscellanea;
+
+public class RomanToInt {
+
+    public static void main(String[] args) {
+        String s = "III";
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char currentChar = s.charAt(i);
+            int currentInt = mapSimpleRomanToInt(currentChar);
+            if (i != s.length() - 1) {
+                char nextChar = s.charAt(i + 1);
+                int nextInt = mapSimpleRomanToInt(nextChar);
+                if (currentChar == 'C' && nextChar == 'M'
+                        || currentChar == 'C' && nextChar == 'D'
+                        || currentChar == 'X' && nextChar == 'L'
+                        || currentChar == 'X' && nextChar == 'C'
+                        || currentChar == 'I' && nextChar == 'V'
+                        || currentChar == 'I' && nextChar == 'X') {
+                    currentInt = nextInt - currentInt;
+                    i++;
+                }
+            }
+            result += currentInt;
+        }
+        System.out.println(result);
+    }
+
+    public static int mapSimpleRomanToInt(char c) {
+        switch (c) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return -1;
+        }
+    }
+}
