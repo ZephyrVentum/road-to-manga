e77cb87ce21974f6f7f7ba6b9d9aa323165efb8b
Oleksandr Klymenko <ikytosan0@gmail.com>
2023-02-19 21:50:27 +0200

    solved leetcode problem 1884

---
+++ algorithms/src/miscellanea/leetcode/EggDropWith2EggsAndNFloors.java
@@ -0,0 +1,14 @@
+package miscellanea.leetcode;
+
+public class EggDropWith2EggsAndNFloors {
+
+    public static void main(String[] args) {
+        System.out.println(new EggDropWith2EggsAndNFloors().twoEggDrop(42));
+
+
+    }
+
+    public int twoEggDrop(int n) {
+        return (int) (0.5 + (Math.sqrt(2*n)));
+    }
+}
