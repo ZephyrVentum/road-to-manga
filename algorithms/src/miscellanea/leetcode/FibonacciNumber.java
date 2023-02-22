4bf2282ac2885082decf2db44c8051c1c7b07ea4
Oleksandr Klymenko <ikytosan0@gmail.com>
2023-02-22 13:46:05 +0200

    solved leetcode problem 509 with sweaty approach

--- algorithms/src/miscellanea/leetcode/FibonacciNumber.java
+++ algorithms/src/miscellanea/leetcode/FibonacciNumber.java
@@ -3,7 +3,7 @@ package miscellanea.leetcode;
 public class FibonacciNumber {
 
     public static void main(String[] args) {
-        System.out.println(new FibonacciNumber().fib(5));
+        System.out.println(new FibonacciNumber().fib2(5));
     }
 
     public int fib(int n) {
@@ -16,5 +16,9 @@ public class FibonacciNumber {
             answer = fib(n - 1) + fib(n - 2);
         }
         return answer;
+    }
+
+    public int fib2(int n) {
+        return (int) ((Math.pow(((1.0 + Math.sqrt(5)) / 2), n) - Math.pow(((1.0 - Math.sqrt(5)) / 2), n)) / Math.sqrt(5));
     }
 }
