890fc7ad8562ad374e2a287f9dcff52475322fe5
Oleksandr Klymenko <ikytosan0@gmail.com>
2023-02-24 17:49:51 +0200

    solved leetcode problem 98

--- algorithms/src/miscellanea/leetcode/ValidateBinarySearchTree.java
+++ algorithms/src/miscellanea/leetcode/ValidateBinarySearchTree.java
@@ -5,34 +5,20 @@ import utils.TreeNode;
 public class ValidateBinarySearchTree {
 
     public static void main(String[] args) {
-        Integer[] tree = new Integer[]{3, 1, 5, 0, 2, 4, 6};
-        TreeNode treeNode = TreeNode.arrayToTree(tree);
+        Integer[] tree = new Integer[]{3,1,5,0,2,4,6,null,null,null,3};
+//        TreeNode treeNode = TreeNode.newTree(tree);
+        TreeNode treeNode = new TreeNode(98, new TreeNode(-57, null, new TreeNode(58, new TreeNode(31), null)), null);
         System.out.println(new ValidateBinarySearchTree().isValidBST(treeNode));
     }
 
     public boolean isValidBST(TreeNode root) {
-        return dfs(root, root);
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
 
-    public boolean dfs(TreeNode root, TreeNode startingVertex) {
-        boolean answer = true;
-        if (root.left != null) {
-            if (root.left.val < root.val && (root.left == startingVertex.left || root.left.val > startingVertex.left.val)) {
-                if (startingVertex == null){
-
-                }
-                answer &= dfs(root.left, startingVertex);
-            } else {
-                return false;
-            }
-        }
-        if (root.right != null) {
-            if (root.right.val > root.val && (root.right == startingVertex.right || root.right.val < startingVertex.right.val)) {
-                answer &= dfs(root.right, startingVertex);
-            } else {
-                return false;
-            }
-        }
-        return answer;
+    public boolean isValidBST(TreeNode root, long minVal, long maxVal) {
+        if (root == null) return true;
+        if (root.val >= maxVal || root.val <= minVal) return false;
+        return isValidBST(root.left, minVal, root.val) && isValidBST(root.right, root.val, maxVal);
     }
+
 }
--- algorithms/src/utils/TreeNode.java
+++ algorithms/src/utils/TreeNode.java
@@ -1,12 +1,11 @@
 package utils;
 
+import java.util.Objects;
+
 public class TreeNode {
     public int val;
     public TreeNode left;
     public TreeNode right;
-
-    public TreeNode() {
-    }
 
     public TreeNode(int val) {
         this.val = val;
@@ -18,15 +17,46 @@ public class TreeNode {
         this.right = right;
     }
 
-    public static TreeNode arrayToTree(Integer array[]) {
-        return arrayToTree(array, 0);
+    public static TreeNode newTree(Integer... vars) {
+        return newTree(0, vars);
     }
 
-    public static TreeNode arrayToTree(Integer array[], int index) {
-        if (index >= array.length)
-            return null;
-        if (array[index] == null)
-            return null;
-        return new TreeNode(array[index], arrayToTree(array, index * 2 + 1), arrayToTree(array, index * 2 + 2));
+    private static TreeNode newTree(int index, Integer... vars) {
+        if (index >= vars.length || vars[index] == null) return null;
+
+        TreeNode node = new TreeNode(vars[index]);
+        node.left = newTree(2 * index + 1, vars);
+        node.right = newTree(2 * index + 2, vars);
+
+        return node;
+    }
+
+    @Override
+    public String toString() {
+        return "TreeNode{" +
+                "val=" + val +
+                ", left=" + left +
+                ", right=" + right +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TreeNode treeNode = (TreeNode) o;
+
+        if (val != treeNode.val) return false;
+        if (!Objects.equals(left, treeNode.left)) return false;
+        return Objects.equals(right, treeNode.right);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = val;
+        result = 31 * result + (left != null ? left.hashCode() : 0);
+        result = 31 * result + (right != null ? right.hashCode() : 0);
+        return result;
     }
 }
