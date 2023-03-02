9406964ecacc5531dd8811e4bcf819572109886e
Oleksandr Klymenko <ikytosan0@gmail.com>
2023-03-02 21:38:19 +0200

    started solving leetcode problem 424

---
+++ algorithms/src/miscellanea/leetcode/LongestRepeatingCharacterReplacement.java
@@ -0,0 +1,11 @@
+package miscellanea.leetcode;
+
+public class LongestRepeatingCharacterReplacement {
+    public static void main(String[] args) {
+        System.out.println(new LongestRepeatingCharacterReplacement().characterReplacement("AABABBA", 1));
+    }
+
+    public int characterReplacement(String s, int k) {
+        return 1;
+    }
+}
