c5d1fc59d244e18cf38d67bc47b6b5fa58a86b08
Oleksandr Klymenko <ikytosan0@gmail.com>
2023-03-03 11:44:36 +0200

    almost solved leetcode problem 424

--- algorithms/src/miscellanea/leetcode/LongestRepeatingCharacterReplacement.java
+++ algorithms/src/miscellanea/leetcode/LongestRepeatingCharacterReplacement.java
@@ -1,11 +1,48 @@
 package miscellanea.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LongestRepeatingCharacterReplacement {
+
     public static void main(String[] args) {
-        System.out.println(new LongestRepeatingCharacterReplacement().characterReplacement("AABABBA", 1));
+        System.out.println(new LongestRepeatingCharacterReplacement().characterReplacement("BAXXXAZAAZZAAXX", 2));
+    }
+
+    static class MetaChar {
+        public MetaChar(int remainingK, int fromIndex, int toIndex) {
+            this.remainingK = remainingK;
+            this.fromIndex = fromIndex;
+            this.toIndex = toIndex;
+        }
+
+        public int remainingK;
+        public int fromIndex;
+        public int toIndex;
     }
 
     public int characterReplacement(String s, int k) {
-        return 1;
+        Map<Character, MetaChar> sMap = new HashMap<>();
+        int max = 0;
+        char[] sChars = s.toCharArray();
+        for (int i = 0; i < sChars.length; i++) {
+            char currentChar = sChars[i];
+            if (currentChar == 'S'){
+                System.out.println("kek");
+            }
+            MetaChar metaChar = sMap.getOrDefault(currentChar, new MetaChar(k, i - 1, i - 1));
+            int newRemainingK = metaChar.remainingK - (i - (metaChar.toIndex + 1));
+            if (newRemainingK >= 0) {
+                int newMax = newRemainingK + (i - metaChar.fromIndex);
+                max = Math.max(newMax, max);
+                metaChar.remainingK = newRemainingK;
+            } else {
+                metaChar.remainingK = k;
+                metaChar.fromIndex = i - 1;
+            }
+            metaChar.toIndex = i;
+            sMap.put(currentChar, metaChar);
+        }
+        return Math.min(max, s.length());
     }
 }
