cee9a7a09d1c1f204b28f815392224fb6474aceb
Oleksandr Klymenko <ikytosan0@gmail.com>
2023-03-09 09:23:18 +0200

    solved leetcode problem 692

--- algorithms/src/miscellanea/leetcode/TopKFrequentWords.java
+++ algorithms/src/miscellanea/leetcode/TopKFrequentWords.java
@@ -5,21 +5,29 @@ import java.util.*;
 public class TopKFrequentWords {
 
     public static void main(String[] args) {
-        System.out.println(new TopKFrequentWords().topKFrequent(new String[]{"the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"}, 4));
+        System.out.println(new TopKFrequentWords().topKFrequent(new String[]{"the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"}, 2));
     }
 
     public List<String> topKFrequent(String[] words, int k) {
         List<String> answer = new ArrayList<>();
         HashMap<String, Integer> frequency = new HashMap<>();
-        PriorityQueue<String> priorityQueue = new PriorityQueue<>(Comparator.reverseOrder());
+        PriorityQueue<Map.Entry<String, Integer>> priorityQueue = new PriorityQueue<>((o1, o2) -> {
+            int frequencyDiff = o2.getValue() - o1.getValue();
+            return frequencyDiff == 0 ? o1.getKey().compareTo(o2.getKey()) : frequencyDiff;
+        });
         for (String word : words) {
             Integer currentFrequency = frequency.getOrDefault(word, 0);
             currentFrequency++;
             frequency.put(word, currentFrequency);
-            priorityQueue.add(word);
         }
-        while (!priorityQueue.isEmpty()){
-            System.out.println(priorityQueue.poll());
+        priorityQueue.addAll(frequency.entrySet());
+        String distinctWord = "";
+        while (!priorityQueue.isEmpty() && answer.size() < k) {
+            String currentWord = priorityQueue.poll().getKey();
+            if (!distinctWord.equals(currentWord)) {
+                answer.add(currentWord);
+                distinctWord = currentWord;
+            }
         }
         return answer;
     }
