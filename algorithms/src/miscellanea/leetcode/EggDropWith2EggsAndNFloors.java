f9a6dd7e43c9fdee5d952727070536755d6eac28
Oleksandr Klymenko <ikytosan0@gmail.com>
2023-03-25 18:57:01 +0200

    added another formula for TwoEggDrop

--- algorithms/src/miscellanea/leetcode/EggDropWith2EggsAndNFloors.java
+++ algorithms/src/miscellanea/leetcode/EggDropWith2EggsAndNFloors.java
@@ -4,9 +4,14 @@ public class EggDropWith2EggsAndNFloors {
 
     public static void main(String[] args) {
         System.out.println(new EggDropWith2EggsAndNFloors().twoEggDrop(42));
+        System.out.println(new EggDropWith2EggsAndNFloors().twoEggDrop2(42));
     }
 
     public int twoEggDrop(int n) {
-        return (int) (0.5 + (Math.sqrt(2*n)));
+        return (int) (0.5 + (Math.sqrt(2 * n)));
+    }
+
+    public int twoEggDrop2(int n) {
+        return (int) Math.ceil((-1.0 + Math.sqrt(1 + 8 * n)) / 2.0);
     }
 }
