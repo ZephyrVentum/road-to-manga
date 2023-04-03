290e20278b474ba49c062513cd02315e4b7321c3
Oleksandr Klymenko <ikytosan0@gmail.com>
2023-04-03 09:44:00 +0300

    solved leetcode promblem 56

--- algorithms/src/miscellanea/leetcode/MergeIntervals.java
+++ algorithms/src/miscellanea/leetcode/MergeIntervals.java
@@ -20,31 +20,38 @@ public class MergeIntervals {
     public int[][] merge(int[][] intervals) {
         int[][] answer;
         List<int[]> tempAnswer = new ArrayList<>();
-        int[] intervalsLength = new int[10000];
+        //10000 from the problem's constraint
+        int[] intervalEdges = new int[10000];
+        int maxStartOfInterval = 0;
         for (int i = 0; i < intervals.length; i++) {
             int startOfInterval = intervals[i][0];
             int endOfInterval = intervals[i][1];
-            intervalsLength[startOfInterval] = Math.max(intervalsLength[startOfInterval], (endOfInterval - startOfInterval) + 1);
+            // use Math.max for case [2,3][2,2][3,4] when you don't want override [2,3] with shorter [2,2].
+            // add +1 for cases like this [0,0][1,1][2,2][3,3] and so far and so on.
+            intervalEdges[startOfInterval] = Math.max(intervalEdges[startOfInterval], endOfInterval + 1);
         }
         int[] tempInterval = new int[]{-1, -1};
-        for (int startOfCurrentInterval = 0; startOfCurrentInterval < intervalsLength.length; startOfCurrentInterval++) {
+        for (int startOfCurrentInterval = 0; startOfCurrentInterval < intervalEdges.length; startOfCurrentInterval++) {
             int endOfTempInterval = tempInterval[1];
-            int currentIntervalLength = intervalsLength[startOfCurrentInterval];
-            if (currentIntervalLength != 0) {
+            int endOfCurrentInterval = intervalEdges[startOfCurrentInterval];
+            if (endOfCurrentInterval != 0) {
                 if (endOfTempInterval != -1) {
                     if (startOfCurrentInterval <= endOfTempInterval) {
-                        endOfTempInterval = Math.max(endOfTempInterval, startOfCurrentInterval + (intervalsLength[startOfCurrentInterval] - 1));
+                        // don't forget to do -1 since we've done +1 in the first loop
+                        endOfTempInterval = Math.max(endOfTempInterval, endOfCurrentInterval - 1);
                         tempInterval[1] = endOfTempInterval;
                     } else {
                         tempAnswer.add(tempInterval);
+                        // don't forget to do -1 since we've done +1 in the first loop
                         tempInterval = new int[]{
                                 startOfCurrentInterval,
-                                startOfCurrentInterval + (intervalsLength[startOfCurrentInterval] - 1)
+                                endOfCurrentInterval - 1
                         };
                     }
                 } else {
                     tempInterval[0] = startOfCurrentInterval;
-                    tempInterval[1] = startOfCurrentInterval + (intervalsLength[startOfCurrentInterval] - 1);
+                    // don't forget to do -1 since we've done +1 in the first loop
+                    tempInterval[1] = endOfCurrentInterval - 1;
                 }
             }
         }
